cebbd0ed684df0a05f3a33c12941eaa825e1d961
rajhans468 <rajhans468@gmail.com>
2020-05-12 22:40:21 +0400

    updated app v2

--- version/app.js
+++ version/app.js
@@ -2,13 +2,13 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World! V2\n');
 });
 
 app.get('/mars', function(req, res){
-    res.send('Hello Mars! \n');
+    res.send('Hello Mars! v2 \n');
 });
 
-app.listen(8888, function () {
-  console.log('Example app listening on port 8888!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
